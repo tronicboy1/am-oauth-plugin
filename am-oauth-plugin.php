9ceb8c84cea9c22893876f5e6350816284c5c13d
Austin John Mayer (井上 オースティン) <mayeraus41@gmail.com>
2023-05-20 07:00:13 +0900

    init commit

---
+++ am-oauth-plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name:       OAuth Plugin
+ * Description:       A plugin which allows third party OAuth in WordPress
+ * Requires at least: 6.2
+ * Requires PHP:      8.0.0
+ * Version:           0.0.0
+ * Author:            Austin J. Mayer
+ * License:           MIT
+ * Text Domain:       am-oauth-plugin
+ */
